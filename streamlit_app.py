4d1e2e4bbcc483c53e6c762493c9ec36852d9103
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-07-25 18:06:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import requests
-st.set_page_config(page_title='Books Store', layout='wide')
-st.markdown(
-        """
-        <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge_1yB5, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+
 
     
 books_Asset_url ="https://github.com/iamanimesh11/intelligence_books_suggester_App/releases/download/Books.pkl/books.pkl"
@@ -60,22 +48,15 @@ def is_valid_input(user_input):
     return user_input in pt.index
 
 def main():
-    st.markdown(
-        """
-        <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge_1yB5, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
     st.set_page_config(page_title='Books Store', layout='wide')
-    
-
+    hide_main_menu_css ="""
+    #MainMenu {
+         visibility:hidden;
+    }
+    """
+    st.markdown(hide_main_menu_css,unsafe_allow_html=True)
+        
     page_options = ['Home', 'ML model']
     selected_page = st.sidebar.selectbox('Navigate to:', page_options)
     st.markdown(
